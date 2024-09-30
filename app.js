4352d701f676d7ba3960b3f910ecd87531fdf4e8
GLFABILA <glfabila2@gmail.com>
2024-09-30 14:11:54 +0800

    added conditional statements

--- app.js
+++ app.js
@@ -6,3 +6,11 @@ let boolean = true;
 // Arithmetic and Logical Operators
 let result = number * 5 + 2;
 let isTrue = number > 5 && boolean;
+
+// Conditional Statements
+// if else
+if (number % 2 === 0) {
+    console.log("Number is even");
+} else {
+    console.log("Number is odd");
+}
